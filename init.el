09c05eea9f1a106676f8b8eb1d1800f02522a352
David A. Ventimiglia <davidaventimiglia@gmail.com>
2024-07-02 11:14:13 -0700

    Added diagnostic switches

--- init.el
+++ init.el
@@ -1,1 +1,5 @@
 (use-package dart-mode :hook (dart-mode) :ensure)
+
+(with-eval-after-load 'eglot
+  (add-to-list 'eglot-server-programs
+	       '(dart-mode . ("dart" "language-server" "--client-id" "emacs.eglot-dart" "--diagnostic-port" "8085" "--protocol-traffic-log" "protocol-traffic.log" "--analysis-driver-log" "analysis-driver.log"))))
