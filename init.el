52862b4933e85c64508de53906044b043d31169e
David A. Ventimiglia <davidaventimiglia@gmail.com>
2024-07-02 15:45:33 -0700

    Updated the README

--- init.el
+++ init.el
@@ -1,11 +1,6 @@
 (use-package dart-mode :hook (dart-mode) :ensure)
 
-;; (with-eval-after-load 'eglot
-;;   (add-to-list 'eglot-server-programs
-;; 	       '(dart-mode . (
-;; 			      "dart" "language-server"
-;; 			      "--client-id" "emacs.eglot-dart"
-;; 			      "--protocol-traffic-log=/dev/stderr"
-;; 			      ))))
+(add-hook 'eglot-managed-mode-hook
+	  (lambda ()
+	    (setq jsonrpc-default-request-timeout 30)))
 
-(setq jsonrpc-default-request-timeout 30)
