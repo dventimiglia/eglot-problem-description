c5f5a176130578c01eefa2f25dedf881f68a100e
David A. Ventimiglia <davidaventimiglia@gmail.com>
2024-07-02 12:17:26 -0700

    Updated the README

--- init.el
+++ init.el
@@ -2,4 +2,8 @@
 
 (with-eval-after-load 'eglot
   (add-to-list 'eglot-server-programs
-	       '(dart-mode . ("dart" "language-server" "--client-id" "emacs.eglot-dart" "--diagnostic-port" "8085" "--protocol-traffic-log" "protocol-traffic.log" "--analysis-driver-log" "analysis-driver.log"))))
+	       '(dart-mode . (
+			      "dart" "language-server"
+			      "--client-id" "emacs.eglot-dart"
+			      "--protocol-traffic-log=/dev/stderr"
+			      ))))
